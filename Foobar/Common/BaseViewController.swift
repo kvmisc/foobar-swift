06c06e1606e56ccb8a2cecc679505151928d2911
Haiping Wu <371288107@qq.com>
2019-12-12 17:25:52 +0800

    At 12121725

---
+++ Foobar/Common/BaseViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewController.swift
+//  Foobar
+//
+//  Created by Kevin Wu on 12/12/19.
+//  Copyright (c) firefly.com. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
