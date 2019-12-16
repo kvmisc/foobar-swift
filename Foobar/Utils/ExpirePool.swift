a5125fab0d92fbcf51395363ec3ef6b113f45b7c
Haiping Wu <371288107@qq.com>
2019-12-16 19:34:59 +0800

    At 12161934

--- Foobar/Utils/ExpirePool.swift
+++ Foobar/Utils/ExpirePool.swift
@@ -9,10 +9,58 @@
 import UIKit
 
 class ExpirePool {
-  static let shared = ExpirePool()
+  fileprivate var path: String = ""
+  fileprivate var pool: [String:Date] = [:]
+  fileprivate var lock = NSLock()
 
-  fileprivate var pool: [String:AnyObject] = [:]
+  init(_ path: String) {
+    self.path = path
 
-  init() {
+    let original = Archive.fromJSONFile(path) as? [String:Date]
+    clean(original)
   }
+
+  func hasKey(_ key: String) -> Bool {
+    var ret = false
+    lock.lock()
+    if let value = pool[key] {
+      ret = Date() < value
+    }
+    lock.unlock()
+    return ret
+  }
+  func setKey(_ key: String, _ timeout: TimeInterval) {
+    lock.lock()
+    if timeout > 0.0 {
+      if !key.isEmpty {
+        pool[key] = Date(timeIntervalSinceNow: timeout)
+      }
+    } else {
+      // ...
+    }
+    lock.unlock()
+  }
+  func removeKey(_ key: String) {
+  }
+  func removeAllKeys() {
+    lock.lock()
+    pool = [:]
+    lock.unlock()
+  }
+
+  func clean(_ source: [String:Date]? = nil) {
+    lock.lock()
+
+    var temp: [String:Date] = [:]
+    let now = Date()
+    for (key, value) in source ?? pool {
+      if key.isEmpty { continue }
+      if value <= now { continue }
+      temp[key] = value
+    }
+    pool = temp
+
+    lock.unlock()
+  }
+
 }
