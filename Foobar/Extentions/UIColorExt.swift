1da5c7394a06a6d229d46433eaace7ab6694e1b8
Haiping Wu <371288107@qq.com>
2020-01-06 17:36:43 +0800

    At 01061736

---
+++ Foobar/Extentions/UIColorExt.swift
@@ -0,0 +1,34 @@
+//
+//  UIColorExt.swift
+//  Foobar
+//
+//  Created by Kevin Wu on 1/6/20.
+//  Copyright (c) firefly.com. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+  func extOverlayWhite() -> UIColor {
+    var bgR: CGFloat = 0
+    var bgG: CGFloat = 0
+    var bgB: CGFloat = 0
+    var bgA: CGFloat = 0
+    self.getRed(&bgR, green: &bgG, blue: &bgB, alpha: &bgA)
+
+    var fgR: CGFloat = 0
+    var fgG: CGFloat = 0
+    var fgB: CGFloat = 0
+    var fgA: CGFloat = 0
+    let overlay = UIColor.white.withAlphaComponent(0.15)
+    overlay.getRed(&fgR, green: &fgG, blue: &fgB, alpha: &fgA)
+
+    let r = fgA * fgR + (1 - fgA) * bgR
+    let g = fgA * fgG + (1 - fgA) * bgG
+    let b = fgA * fgB + (1 - fgA) * bgB
+
+    return UIColor(red: r, green: g, blue: b, alpha: 1.0)
+  }
+
+}
